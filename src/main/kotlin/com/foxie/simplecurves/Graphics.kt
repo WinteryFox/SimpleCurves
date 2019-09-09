1746a53009e3aebdc80b5853f6bd64a709967925
WinteryFox <dragonhel5@gmail.com>
2019-09-09 23:54:56 +0200

    I think it works!

--- src/main/kotlin/com/foxie/simplecurves/Graphics.kt
+++ src/main/kotlin/com/foxie/simplecurves/Graphics.kt
@@ -3,13 +3,14 @@ package com.foxie.simplecurves
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
+import kotlin.math.roundToInt
 
 fun renderGraph(width: Int, height: Int, color: Pixel, points: List<Point>, type: Type): List<List<Pixel>> {
     val graph: List<Point> = interpolateGraph(points, width / (points.size - 1), type)
     val image = BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB)
 
     graph.forEach {
-        val x = it.x.toInt()
+        val x = it.x.roundToInt()
         val y = it.y.toInt()
         image.setRGB(x, height - 1 - y, color.a.shl(24).or(color.r.shl(16)).or(color.g.shl(8)).or(color.b))
     }
--- src/main/kotlin/com/foxie/simplecurves/Interpolater.kt
+++ src/main/kotlin/com/foxie/simplecurves/Interpolater.kt
@@ -25,20 +25,19 @@ fun interpolate(p1: Point, p2: Point, stepCount: Int, type: Type): List<Point> {
     val stepWidth = (p2.x - p1.x) / stepCount
 
     for (step in 0..stepCount) {
+        val x = step * stepWidth
         if (type == Type.LINEAR) {
             val coefficient = (p2.y - p1.y) / (p2.x - p1.x)
-            val x = step * stepWidth
             val y = coefficient * x + p1.y
 
             interpolated.add(Point(x + p1.x, y))
         } else if (type == Type.SMOOTH) {
-            val x = step * stepWidth
             val smooth = smoothStep(0.0, p2.x - p1.x, x)
             val normalized = if (p1.y > p2.y)
                 1.0 - smooth
             else
                 smooth
-            val y = normalized * -(p2.y - p1.x)
+            val y = normalized * if (p2.y - p1.y < 0) -(p2.y - p1.y) else p2.y - p1.y
 
             interpolated.add(Point(x + p1.x, y))
         }
